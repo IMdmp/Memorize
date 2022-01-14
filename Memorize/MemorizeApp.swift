7f84d253df82aca8079588544625df1ff4fbb32d
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-01-14 13:02:54 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nicky on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
