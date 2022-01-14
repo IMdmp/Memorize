2fd08af9a810ce6296bdaae5632b1b803b9d9235
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-01-14 14:15:08 +0800

    lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚅","🚀","✈️","🚁","😊","😇"]
+    
+    @State var emojiCount = 3
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+                ForEach(emojis[0..<emojiCount],id:\.self){ emoji in
+                    CardView(content: emoji ).aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+            }
+            .foregroundColor(.red)
+
+            Spacer()
+            HStack{
+                add
+                Spacer()
+                remove
+            }
+            .padding(.horizontal)
+        }
+       
+        .padding(.horizontal)
+}
+    var remove :some View {
+        Button {
+            if emojiCount>1{
+                emojiCount-=1
+            }
+        } label:{
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add :some View{
+        Button{
+            if emojiCount < emojis.count{
+                emojiCount+=1
+            }
+        } label: {
+            Image(systemName: "plus.circle")}
     }
 }
+struct CardView:View{
+    var content : String
+    @State var isFaceUp : Bool = true
+    
+    var body:some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp{
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content)
+            }else{
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
